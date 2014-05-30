638f9548a3714b88d906a1126c4a529faa6cef5f
Felipe Lerena <felipelerena@gmail.com>
2014-05-30 18:54:45 -0300

    improving parameters

--- touchandgo/touchandgo.py
+++ touchandgo/touchandgo.py
@@ -5,24 +5,39 @@ from torrentmediasearcher import TorrentMediaSearcher
 from subtitles import get_subtitle
 
 
-def main(name, season, episode):
-    print("Obteniendo magnet")
-    search = TorrentMediaSearcher()
-    search.request_tv_magnet(provider='eztv', show=name , season=season,
-                             episode=episode, quality='normal',
-                             callback=get_magnet)
 
-def get_magnet(results):
-    magnet = results['magnet']
-    subtitle = get_subtitle(magnet)
-    command = "peerflix \"%s\" -t /tmp/%s --vlc" % (magnet, subtitle)
-    local(command, capture=False)
+def main(name, season=None, episode=None, sub_lang=None):
+
+    def get_magnet(results):
+        magnet = results['magnet']
+        command = "peerflix \"%s\"" % magnet
+        if sub_lang is not None:
+            print("Obteniendo subtitulo")
+            subtitle = get_subtitle(magnet)
+            command += " -t /tmp/%s" % subtitle
+
+        command += " --vlc"
+
+        local(command, capture=False)
+
+    print("Obteniendo magnet")
+    search = TorrentMediaSearcher
+    if season is None and episode is None:
+        search.request_movie_magnet('torrentproject', name,
+                                    callback=get_magnet)
+    else:
+        search.request_tv_magnet(provider='eztv', show=name,
+                                 season=int(season), episode=int(episode),
+                                 quality='normal', callback=get_magnet)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("name")
-    parser.add_argument("season")
-    parser.add_argument("episode")
+
+    parser.add_argument("sea_ep", nargs='*', default=[None, None])
+    parser.add_argument("--sub", nargs='?', default=None)
     args = parser.parse_args()
 
-    main(args.name, int(args.season), int(args.episode))
+    main(args.name, season=args.sea_ep[0], episode=args.sea_ep[1],
+         sub_lang=args.sub)
