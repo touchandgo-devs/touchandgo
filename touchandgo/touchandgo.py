dc3a79c3f63e11fbd4cb06390c7244c1fd7721b1
Felipe Lerena <felipelerena@gmail.com>
2014-06-06 18:31:37 -0300

    removing fabric

--- touchandgo/touchandgo.py
+++ touchandgo/touchandgo.py
@@ -1,14 +1,42 @@
 #! /usr/bin/env python
 import argparse
+import socket
 
-from fabric.operations import local
 from torrentmediasearcher import TorrentMediaSearcher
 
 from subtitles import get_subtitle
+from subprocess import PIPE, STDOUT, Popen
+import re
+
+
+def get_free_port():
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.bind(('localhost', 0))
+  addr, port = s.getsockname()
+  s.close()
+  return port
+
+
+def execute(command):
+    process = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
+    print("Running peerflix")
+    while True:
+        nextline = process.stdout.readline()
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', nextline)
+        if len(urls):
+            print("streaming url: %s" % urls[0])
+            break
+
+        if nextline == '' and process.poll() != None:
+            break
+
+    output = process.communicate()[0]
+    exitCode = process.returncode
+    print output, exitCode
 
 
 def main(name, season=None, episode=None, sub_lang=None, serve=False,
-         quality=None):
+         quality=None, random_port=False):
 
     def get_magnet(results):
         print("Processing magnet link")
@@ -18,11 +46,13 @@ def main(name, season=None, episode=None, sub_lang=None, serve=False,
             subtitle = get_subtitle(magnet, sub_lang)
             if subtitle is not None:
                 command += " -t %s" % subtitle
-
+        if random_port:
+            command += " -p%s" % get_free_port()
         if not serve:
             command += " --vlc"
 
-        local(command, capture=False)
+        print("executing command %s" % command)
+        execute(command)
 
     print("Searching torrent")
     search = TorrentMediaSearcher
