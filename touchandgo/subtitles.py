ba7af36ab678e6dbcbff886e607b7ad7b10cdb9c
Felipe Lerena <felipelerena@gmail.com>
2014-08-09 12:42:33 -0300

    improving subtitles

--- touchandgo/subtitles.py
+++ touchandgo/subtitles.py
@@ -1,3 +1,4 @@
+from os.path import join
 from time import sleep
 
 from babelfish import Language
@@ -8,10 +9,14 @@ from subliminal.subtitle import get_subtitle_path
 from subliminal.video import Video
 
 
+TMP_DIR = "/tmp"
+
+
 def get_subtitle(magnet, lang):
-    print("Obtaining subtitle (experimental, might take a while)")
+    print("Obtaining subtitle")
     lt_session = session()
-    params = {"save_path": "/tmp"}
+    params = {"save_path": TMP_DIR,
+              "allocation": "compact"}
     handle = add_magnet_uri(lt_session, magnet, params)
 
     while (not handle.has_metadata()):
@@ -26,7 +31,7 @@ def get_subtitle(magnet, lang):
             biggest_file = [file_.path, file_.size]
 
     print("Guessing data")
-    filepath = biggest_file[0]
+    filepath = join(TMP_DIR, biggest_file[0])
     guess = guess_video_info(filepath, info=['filename'])
     video = Video.fromguess(filepath, guess)
     video.size = biggest_file[1]
@@ -35,9 +40,7 @@ def get_subtitle(magnet, lang):
     if not len(subtitle):
         subtitle = None
     else:
-        subtitle = get_subtitle_path(video.name)
+        subtitle = get_subtitle_path(join(TMP_DIR, video.name))
+
     lt_session.remove_torrent(handle)
     return subtitle
-
-
-
