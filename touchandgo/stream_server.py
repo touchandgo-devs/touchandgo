fbe92447db2812d59553a4a2f333d110105e4acb
Felipe Lerena <felipelerena@gmail.com>
2014-08-11 23:46:35 -0300

    now the file streaming server can return more than one request simultaneously

--- touchandgo/stream_server.py
+++ touchandgo/stream_server.py
@@ -1,4 +1,5 @@
-import BaseHTTPServer
+from SocketServer import ThreadingMixIn
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
 from os.path import join
 
@@ -6,24 +7,30 @@ from guessit import guess_video_info
 
 from touchandgo.settings import TMP_DIR
 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+        pass
 
 def serve_file(manager):
 
-    class HTTPHandlerOne(BaseHTTPServer.BaseHTTPRequestHandler):
+    class VideoHandler(BaseHTTPRequestHandler):
         def do_GET(self):
             video = manager.video_file
             video_path = join(TMP_DIR, video[0])
             self.send_response(200)
             guess = guess_video_info(video_path, info=['filename'])
+            #import mimetypes
+            #print mimetypes.read_mime_types(video_path)
+            #print guess
             self.send_header('Content-type', guess["mimetype"])
             self.send_header('Mime-type', guess["mimetype"])
             self.end_headers()
-            self.wfile.write(open(video_path).read())
+            data = open(video_path).read()
+            self.wfile.write(data)
 
-    def run(server_class=BaseHTTPServer.HTTPServer,
-            handler_class=BaseHTTPServer.BaseHTTPRequestHandler):
-        server_address = ('', manager.port)
+    def run(server_class=ThreadedHTTPServer,
+            handler_class=BaseHTTPRequestHandler):
+        server_address = ('0.0.0.0', manager.port)
         httpd = server_class(server_address, handler_class)
         httpd.serve_forever()
 
-    run(handler_class=HTTPHandlerOne)
+    run(handler_class=VideoHandler)
