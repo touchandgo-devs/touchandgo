9c2948ea268b9fea310beb65295f91757b56e214
felipe lerena <felipelerena@gmail.com>
2014-10-28 01:48:39 -0300

    adding pyaml requirement

--- setup.py
+++ setup.py
@@ -25,7 +25,8 @@ setup(
                       'flask',
                       'simplejson',
                       'python-daemon',
-                      'ojota'],
+                      'ojota',
+                      'pyaml'],
     entry_points={
         'console_scripts': ['touchandgo = touchandgo.__init__:main',
                             'tsproxy = touchandgo.server:serve']
