fb205fe8d8e4044c373130dbb5665ba835cf669f
Felipe Lerena <felipe@corrin.lasheras>
2016-07-07 23:04:44 -0300

    updating versions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='Touchandgo',
-    version='0.12.5',
+    version='0.13.0',
     author='Felipe Lerena, Nicolás Demarchi',
     description='A python app and library to watch series magically',
     author_email='felipelerena@gmail.com - mail@gilgamezh.me',
